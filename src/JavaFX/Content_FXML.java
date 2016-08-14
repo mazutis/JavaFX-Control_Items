cc99ef61bacd9afd9e41fd164e6fc3c394456c33
EM <ernestasmazutis@gmail.com>
2016-08-14 23:30:47 +0200

    FXML Playground
    Content_FXML for the moment works separately from GUI00.

---
+++ src/JavaFX/Content_FXML.java
@@ -0,0 +1,51 @@
+package JavaFX;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+/**
+ * Created by ernes on 10/07/2016.
+ */
+
+public class Content_FXML extends Application {
+
+   /* public static void display () {
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL); //Defines a modal window that blocks events from being delivered to any other application window.
+        window.setTitle("FXML");
+        window.setMinWidth(250);
+        System.out.println ("Menu Item FXML activated");
+
+
+
+
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(            );
+        layout.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }*/
+
+    /*public static void main (String[] args) {
+        launch(args);
+    }*/
+
+    @Override
+    public void start (Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Content_FXML.fxml"));
+        primaryStage.setTitle("FXML Playground");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+}
---
+++ src/JavaFX/Controller_FXML.java
@@ -0,0 +1,7 @@
+package JavaFX;
+
+/**
+ * Created by ernes on 14/08/2016.
+ */
+public class Controller_FXML {
+}
